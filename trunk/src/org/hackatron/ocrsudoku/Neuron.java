a73c975e24802d2beb78bd11567f85441f084a4c
plutonia3k <plutonia3k@39aa475c-7230-11de-a9df-91707ec8e704>
2009-07-16 20:50:47 +0000

    Added OCR code

---
+++ trunk/src/org/hackatron/ocrsudoku/Neuron.java
@@ -0,0 +1,71 @@
+package org.hackatron.ocrsudoku;
+
+import java.util.Random;
+
+public class Neuron {
+	private int _width;
+	private int _height;
+
+	// The character that the neuron has learned.
+	public char _character;
+
+	// Initializes the neuron weights and inputs
+	private float[][] _weights;
+
+	// Learning rate.
+	private float _eta;
+
+	// Creates an random method.
+	private Random _rand;
+
+	// Neuron constructor.
+	// Sets the learning rate and initializes the neuron.
+	public Neuron(char character, int width, int height) {
+
+		_character = character;
+
+		_eta = 0.20f;
+		_rand = new Random(System.nanoTime());
+
+		_weights = new float[width][height];
+
+		initNeuron();
+	}
+
+	// Sets the weights to random floats with values 0 - 1
+	private void initNeuron() {
+		for (int row = 0; row < _height; row++) {
+			for (int column = 0; column < _width; column++) {
+				_weights[column][row] = ((float) _rand.nextFloat());
+			}
+		}
+	}
+
+	// Gets the character
+	public char getCharacter() {
+		return _character;
+	}
+
+	// Calculates the output from the neuron
+	// Multiplies each weight with input and returns the sum
+	public float getOutput(int[][] input) {
+		float output = 0;
+		for (int row = 0; row < _height; row++) {
+			for (int column = 0; column < _width; column++) {
+				output += input[column][row] * _weights[column][row];
+			}
+		}
+		return output;
+	}
+
+	// Learns the neuron
+	// Inputs error. If 1, positive learning and if -1, negative learning.
+	public void doLearn(int[][] input, int error) {
+		for (int row = 0; row < _height; row++) {
+			for (int column = 0; column < _width; column++) {
+				// Calculates the new weights.
+				_weights[column][row] += input[column][row] * _eta * error;
+			}
+		}
+	}
+}
---
+++ trunk/src/org/hackatron/ocrsudoku/OpticalCharacterRecognition.java
@@ -0,0 +1,66 @@
+package org.hackatron.ocrsudoku;
+
+import java.util.ArrayList;
+
+public class OpticalCharacterRecognition {
+	private int _width;
+	private int _height;
+
+	private ArrayList<Neuron> _neuronList;
+
+	public OpticalCharacterRecognition(int width, int height) {
+		_width = width;
+		_height = height;
+		_neuronList = new ArrayList<Neuron>();
+	}
+
+	// Learns a character.
+	// Gets the pixel values and the character to be learned as input.
+	// Returns true if character already learned.
+	public boolean doLearn(int[][] input, char character) {
+		for (Neuron neuron : _neuronList) {
+			// Finds if character already is in the list.
+			if (neuron.getCharacter() == character) {
+				// Learn
+				neuron.doLearn(input, 1);
+				return true;
+			}
+		}
+
+		// Creates a new neuron if character not already in list.
+		Neuron neuron = new Neuron(character, _width, _height);
+		neuron.doLearn(input, 1);
+
+		// Adds the neuron to the list.
+		_neuronList.add(neuron);
+
+		return false;
+	}
+
+	// Finds the best matching neuron.
+	// Gets the pixel values as input.
+	// Returns a string of the best match
+	public char getBestMatch(int[][] input) {
+
+		if (_neuronList.size() == 0) {
+			return ' ';
+		}
+
+		Neuron bestNeuron = _neuronList.get(0);
+		float bestValue = bestNeuron.getOutput(input);
+
+		// For each neuron in the list the best match is found to the input
+		// pixel values
+		for (int index = 1; index < _neuronList.size(); index++) {
+			Neuron currentNeuron = _neuronList.get(index);
+			float currentValue = currentNeuron.getOutput(input);
+
+			if (currentValue > bestValue) {
+				bestNeuron = currentNeuron;
+				bestValue = currentValue;
+			}
+		}
+
+		return bestNeuron.getCharacter();
+	}
+}
