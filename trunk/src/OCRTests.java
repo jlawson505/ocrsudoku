4474cea4b32b0f3f9b4b4151d7c19b8ccdbf8c38
plutonia3k <plutonia3k@39aa475c-7230-11de-a9df-91707ec8e704>
2009-07-16 19:07:09 +0000

    Added test class

---
+++ trunk/src/OCRTests.java
@@ -0,0 +1,13 @@
+import junit.framework.TestCase;
+
+public class OCRTests extends TestCase {
+
+	public OCRTests(String name) {
+		super(name);
+	}
+
+	public void testSimpleTest() {
+		int answer = 2;
+		assertEquals((1 + 1), answer);
+	}
+}
