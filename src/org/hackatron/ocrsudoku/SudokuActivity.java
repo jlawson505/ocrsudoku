5d9d0ebd6e81c3c3a8ea45762cd6925e965d4fb7
chris.do.sorensen <chris.do.sorensen@39aa475c-7230-11de-a9df-91707ec8e704>
2009-08-19 21:33:49 +0000

    Improved rendering of text

--- src/org/hackatron/ocrsudoku/SudokuActivity.java
+++ src/org/hackatron/ocrsudoku/SudokuActivity.java
@@ -1,11 +1,5 @@
 package org.hackatron.ocrsudoku;
 
-import javax.microedition.khronos.opengles.GL;
-
-import org.hackatron.ocrsudoku.R;
-import org.hackatron.ocrsudoku.R.layout;
-import org.hackatron.ocrsudoku.graphics.MatrixTrackingGL;
-
 import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Context;
@@ -24,11 +18,10 @@ import android.view.View;
 import android.view.View.OnKeyListener;
 import android.opengl.GLSurfaceView;
 
-
 public class SudokuActivity extends Activity {
 	final static String TAG = "Sudoku";
 	private ISolverService mSolverService = null;
-	private GLSurfaceView _glView;
+	private HUDView _hudView;
 
     private ServiceConnection mConnection = new ServiceConnection() {
 		@Override
@@ -67,27 +60,9 @@ public class SudokuActivity extends Activity {
         
         //setContentView(R.layout.main);
         
-        _glView = new GLSurfaceView(this);
-        _glView.setGLWrapper(new GLSurfaceView.GLWrapper() {
-            public GL wrap(GL gl) {
-                return new MatrixTrackingGL(gl);
-            }});
-        _glView.setRenderer(new HUDView(this));
-        setContentView(_glView);
+        _hudView = new HUDView(this);
+        setContentView(_hudView);
         
     }
-    
-    @Override
-    protected void onPause() {
-        super.onPause();
-        _glView.onPause();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        _glView.onResume();
-    }
-
 	    
 }
